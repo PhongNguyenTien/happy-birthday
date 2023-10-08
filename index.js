84b021cee43615832d7641598687c53875c627dd
PhongNguyenTien <99393835+PhongNguyenTien@users.noreply.github.com>
2023-10-08 21:57:33 +0700

    xong

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const date = new Date();
 
-if (date.getDay == 8) {
-    document.body = "Please wait until 0:00";
+if (date.getDate() == 8 && date.getMonth() == 9) {
+  document.querySelector(
+    ".container"
+  ).innerHTML = `<h1> Đợi đến 0h đi bà nội :) `;
+} else {
+  document.querySelector(".container").innerHTML = `<div class="card">
+    <div class="outside">
+      <div class="front">
+        <p>Happy Birthday Phuong Linh ^^</p>
+        <div class="cake">
+          <div class="top-layer"></div>
+          <div class="middle-layer"></div>
+          <div class="bottom-layer"></div>
+          <div class="candle"></div>
+        </div>
+      </div>
+      <div class="back"></div>
+    </div>
+    <div class="inside">
+      <p>Chúc chị tuổi mới zui zẻ hơn, xinh đẹp hơn, luôn bình an và hạnh phúc trên chặng đường sắp tới nha &#128156</p>
+      <p>P/s: nhấn vô hộp quà để thấy quà ^^</p>
+      <h1><a href="https://phongnguyentien.github.io/happy-birthday/gift.html">&#127873;</a></h1>
+    </div>
+  </div>`;
 }
